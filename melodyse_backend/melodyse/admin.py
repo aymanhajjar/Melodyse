8e18a5cd4ab691a8ab0c5d2856559217d3d45b6f
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-13 18:18:18 +0300

    registered models on admin site

--- melodyse_backend/melodyse/admin.py
+++ melodyse_backend/melodyse/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(UserInfo)
+admin.site.register(UserFriend)
+admin.site.register(FriendRequest)
+admin.site.register(UserRating)
+admin.site.register(Project)
+admin.site.register(File)
+admin.site.register(Task)
+admin.site.register(Notification)
+admin.site.register(Track)
+admin.site.register(TrackComment)
