8adadfbf19e7800f91915bc9f46b3856fc57b8ba
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-19 16:37:08 +0300

    arranged request menu

--- melodyse_frontend/src/components/FriendButton/FriendButton.tsx
+++ melodyse_frontend/src/components/FriendButton/FriendButton.tsx
@@ -2,6 +2,7 @@ import styles from './friendButton.module.scss'
 import { useEffect, useState } from 'react'
 import FormInput from '../FormInput/FormInput'
 import axios from 'axios'
+import Request from './Request'
 import Cookies from 'js-cookie'
 
 export default function FriendButton(props: any) {
@@ -53,24 +54,14 @@ export default function FriendButton(props: any) {
                                 className={friendsTab ? styles.reqBtnInactive : styles.reqBtnActive}
                                 onClick={() => setFriendsTab(!friendsTab)}>Projects</button>
                         </div>
-                        {requests ? requests.map(chat => (
-                            <div className={chat.number_unread > 0 ? styles.chatUnread : styles.chatRead}>
-                                <img className={styles.profPic} src={process.env.SITE_URL + chat.participant_img}/>
-                                <div className={styles.details}>
-                                <h3>{chat.participant}</h3>
-                                <span>{chat.user_id == chat.latest_message.author_id 
-                                        && (chat.latest_message.is_read ? <img className={styles.checkMark} src={'/msg-read.png'}/> : <img className={styles.checkMark} src={'/msg-unread.png'}/>) }
-                                        {chat.latest_message.content}
-                                        </span>
-                                </div>
-                                {chat.number_unread > 0 && <div className={styles.unreadNumber}>
-                                    {chat.number_unread}
-                                </div>}
-                            </div>
-                        ))
-                        
-                        :
-                        <span className={styles.noChat}>You don't have any friend requests</span>}
+
+                        { friendsTab && (requests.friends ? requests.friends.map(req => (
+                                    <Request type='friend' request={req}/>
+                                )) :   <span className={styles.noChat}>You don't have any friend requests</span>)}
+
+                        { !friendsTab && (requests.projects ? requests.projects.map(req => (
+                                    <Request type='project' request={req}/>
+                                )) :   <span className={styles.noChat}>You don't have any project requests</span>)}
                     </div>
                 </div>
                 }
