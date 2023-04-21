76abca851990679d43f8fa3c3a24b75a73984283
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-21 11:05:27 +0300

    added component

---
+++ melodyse_frontend/src/components/FavoriteArtists/FavoriteArtists.tsx
@@ -0,0 +1,11 @@
+import styles from './FavoriteArtists.module.scss'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function FavoriteArtists(props: any) {
+
+    return(
+        <div className={styles.container}>
+        </div>
+    )
+}
