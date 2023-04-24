6e30c99d802426cc93a93a0d23208339b3be93a1
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-24 22:49:15 +0300

    backend: included assistant urls

--- melodyse_backend/melodyse_backend/urls.py
+++ melodyse_backend/melodyse_backend/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls"))
+    path("", include("users.urls")),
+    path("", include("assistant.urls"))
 ]
 
 if settings.DEBUG:
