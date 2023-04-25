43c51ecee1201b3fa6010dfa170c340a9e23f7f6
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-25 16:53:14 +0300

    backend: added views

--- melodyse_backend/assistant/views.py
+++ melodyse_backend/assistant/views.py
@@ -75,7 +75,46 @@ def grammar(request):
         return HttpResponse('User not logged in', status=403)
 
 def generate(request):
-    pass
+    if request.user.is_authenticated:
+
+        lyrics = request.POST['lyrics']
+        with_interests = request.POST['with_interests'].lower() == "true"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, lyrics=lyrics, with_interests=with_interests, type="generate"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
+
 
 def generateMelody(request):
-    pass
+    if request.user.is_authenticated:
+
+        lyrics = request.POST['lyrics']
+        with_interests = request.POST['with_interests'].lower() == "true"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, lyrics=lyrics, with_interests=with_interests, type="generateMelody"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
