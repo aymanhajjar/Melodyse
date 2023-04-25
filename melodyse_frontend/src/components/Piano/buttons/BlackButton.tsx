2077b6893b16630ba84b184960cf8931e2016f6a
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-25 17:47:18 +0300

    frontend: added black and white buttons

---
+++ melodyse_frontend/src/components/Piano/buttons/BlackButton.tsx
@@ -0,0 +1,12 @@
+import styles from './BlackButton.module.scss'
+import { useEffect, useState} from 'react'
+
+export default function BlackButton() {
+
+  return (
+    <>
+      <div className={styles.blackButton}>
+      </div>
+    </>
+  )
+}
---
+++ melodyse_frontend/src/components/Piano/buttons/WhiteButton.tsx
@@ -0,0 +1,12 @@
+import styles from './WhiteButton.module.scss'
+import { useEffect, useState} from 'react'
+
+export default function WhiteButton() {
+
+  return (
+    <>
+      <div className={styles.whiteButton}>
+      </div>
+    </>
+  )
+}
