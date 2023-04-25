48f83f18f9c922fed95cd09bf8e9ff3f86aa03f1
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-25 17:07:59 +0300

    backend: fixed improve lyrics prompt

--- melodyse_backend/assistant/modules/prompt.py
+++ melodyse_backend/assistant/modules/prompt.py
@@ -15,9 +15,9 @@ def genPrompt(user, with_interests, type, lyrics=None):
     
     if type == "improve":
         if with_interests:
-            return "For this prompt, I want you to assume that you are a songwriter who likes " + ', '.join([str(artist['name']) for artist in info.favorite_artists[:5]]) + ' and whose favorite songs include ' + ', '.join([str(song['name']) for song in info.favorite_songs[:5]]) + '. Keeping this in mind, I want you to improve on these lyrics, getting inspiration from your favorite songs and artists. If a part sounds good, leave it as it is. In your response, I only want you to return the improved lyrics, no text before or after or other than the improved lyrics. Here are the lyrics: ' + lyrics
+            return "For this prompt, I want you to assume that you are a songwriter who likes " + ', '.join([str(artist['name']) for artist in info.favorite_artists[:5]]) + ' and whose favorite songs include ' + ', '.join([str(song['name']) for song in info.favorite_songs[:5]]) + '. Keeping this in mind, I want you to improve on these lyrics, getting inspiration from your favorite songs and artists. If a part sounds good, leave it as it is. If a word or sentence can be improved, change it. Do not add more than 6 lines of lyrics, if needed. In your response, I only want you to return the improved lyrics, no text before or after or other than the improved lyrics. Do not label any part. Here are the lyrics: ' + lyrics
         else :
-            return "I want you to improve on these lyrics, semantically and grammatically. If a part sounds good, leave it as it is. In your response, I only want you to return the improved lyrics, no text before or after or other than the improved lyrics. Here are the lyrics: " + lyrics
+            return "I want you to improve on these lyrics, semantically and grammatically. If a part sounds good, leave it as it is. If a word or sentence can be improved, change it. Do not add more than 6 lines of lyrics, if needed. In your response, I only want you to return the improved lyrics, no text before or after or other than the improved lyrics. Do not label any part. Here are the lyrics: " + lyrics
     
     if type == "generate":
         if with_interests:
