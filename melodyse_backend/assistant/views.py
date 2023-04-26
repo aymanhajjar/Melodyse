2c057086aa691ee37068791554c50e4f6eaded2c
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-26 21:27:15 +0300

    backend: added buildsound view

--- melodyse_backend/assistant/views.py
+++ melodyse_backend/assistant/views.py
@@ -118,3 +118,24 @@ def generateMelody(request):
     
     else:
         return HttpResponse('User not logged in', status=403)
+
+def buildSound(request):
+    if request.user.is_authenticated:
+
+        lyrics = request.POST['lyrics']
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, with_interests=False, type="buildsound"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
