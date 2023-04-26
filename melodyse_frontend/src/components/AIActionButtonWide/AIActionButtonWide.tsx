03c44105e11272185b6ae1491cfad30b029dbf2e
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-26 20:53:34 +0300

    frontend: added bright class

--- melodyse_frontend/src/components/AIActionButtonWide/AIActionButtonWide.tsx
+++ melodyse_frontend/src/components/AIActionButtonWide/AIActionButtonWide.tsx
@@ -4,12 +4,20 @@ import { useEffect, useState } from 'react'
 export default function AIActionButtonWide({name, pic, subscription, submit, disabled, bright=null}: any) {
 
     return(
-        <div className={disabled? styles.disabled : styles.aibutton} onClick={submit}>
+        <div 
+            className={disabled? styles.disabled : 
+            bright ? `${styles.aibutton} ${styles.bright}` : styles.aibutton } 
+            onClick={submit}>
+
             <h3>{name}</h3>
             <img src={pic}/>
+
             {subscription && <div style={{backgroundColor: subscription.card_color}} className={styles.subscription}>
+
                     {subscription.tag_on_profile}
+
                 </div>}
+                
         </div>
     )
 }
