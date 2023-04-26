d135369c97e3338701aeec8d56184272c96028ee
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-26 23:10:02 +0300

    backend: added findbass view

--- melodyse_backend/assistant/views.py
+++ melodyse_backend/assistant/views.py
@@ -140,3 +140,24 @@ def buildSound(request):
     
     else:
         return HttpResponse('User not logged in', status=403)
+    
+def findBass(request):
+    if request.user.is_authenticated:
+
+        scale = request.POST['scale']
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, with_interests=False, scale=scale, type="findbass"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
