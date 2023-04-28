c50843c861f12299dc5c4466ddd3cec68479c0ba
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-28 20:30:15 +0300

    backend: added gettips view

--- melodyse_backend/assistant/views.py
+++ melodyse_backend/assistant/views.py
@@ -162,3 +162,25 @@ def findBass(request):
     
     else:
         return HttpResponse('User not logged in', status=403)
+    
+def getTips(request):
+    if request.user.is_authenticated:
+
+        skill = request.POST['skill']
+        with_interests = request.POST['with_interests'].lower() == "true"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, with_interests=with_interests, skill=skill, type="getTips"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
