bc820e796c729a6b66bc9696598531c37779d3dd
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-28 21:40:51 +0300

    frontend: added axios call for explainMusic

--- melodyse_frontend/src/pages/assistant/learning.tsx
+++ melodyse_frontend/src/pages/assistant/learning.tsx
@@ -38,7 +38,10 @@ export default function Learning({ skills } : any) {
     }
 
     const explainMusic = () => {
-
+        axios.get(`${process.env.SITE_URL}/explainMusic?with_interests=${withInterests}`, {
+            withCredentials: true
+        }).then(res => setResponse(res.data.choices[0].text.replace(/^\s+/, "")))
+        .catch(err => console.error(err))
     }
 
     return (
