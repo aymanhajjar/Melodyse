7252913949524477a1a0d1c4bf1110da7b246efc
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-29 01:05:04 +0300

    backend: added view

--- melodyse_backend/assistant/views.py
+++ melodyse_backend/assistant/views.py
@@ -246,7 +246,7 @@ def explainMusic(request):
     else:
         return HttpResponse('User not logged in', status=403)
     
-def suggestTitle(request):
+def findTitle(request):
     if request.user.is_authenticated:
 
         lyrics = request.POST['lyrics']
@@ -256,7 +256,31 @@ def suggestTitle(request):
 
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt= prompt.genPrompt(user=request.user, with_interests=with_interests, genre=genre, mood=mood, type="findTitle"),
+            prompt= prompt.genPrompt(user=request.user, with_interests=with_interests, lyrics=lyrics, genre=genre, mood=mood, type="findTitle"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
+    
+def suggestCover(request):
+    if request.user.is_authenticated:
+
+        lyrics = request.POST['lyrics']
+        genre = request.POST['genre']
+        mood = request.POST['mood']
+        with_interests = request.GET['with_interests'].lower() == "true"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, with_interests=with_interests, lyrics=lyrics, genre=genre, mood=mood, type="suggestCover"),
             temperature=0,
             max_tokens=1000,
             top_p=1.0,
