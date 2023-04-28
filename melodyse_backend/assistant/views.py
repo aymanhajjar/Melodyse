bceadc75d7189e40f29c00d5294bdcbe5755a7d5
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-28 21:43:29 +0300

    backend: added songsToLearn view

--- melodyse_backend/assistant/views.py
+++ melodyse_backend/assistant/views.py
@@ -183,3 +183,24 @@ def getTips(request):
     
     else:
         return HttpResponse('User not logged in', status=403)
+    
+def songsToLearn(request):
+    if request.user.is_authenticated:
+
+        with_interests = request.GET['with_interests'].lower() == "true"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= prompt.genPrompt(user=request.user, with_interests=with_interests, skill=skill, type="getSongsToLearn"),
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\"\"\""]
+        )
+
+        return JsonResponse(response, safe=False)
+    
+    else:
+        return HttpResponse('User not logged in', status=403)
