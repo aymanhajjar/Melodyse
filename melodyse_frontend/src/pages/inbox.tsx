c6041a8f6d6935553939f543d09131931ad1867d
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-04-30 22:35:03 +0300

    frontend: fixed styling

--- melodyse_frontend/src/pages/inbox.tsx
+++ melodyse_frontend/src/pages/inbox.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import styles from '@/styles/Inbox.module.scss'
-import { useState,useEffect } from 'react'
+import { useState,useEffect, useRef } from 'react'
 import axios from 'axios'
 import ChatCard from '@/components/ChatCard/ChatCard'
 import ChatInput from '@/components/ChatInput/ChatInput'
 import Message from '@/components/Message/Message'
 
-export default function Inbox({inbox, userData} : any) {
+export default function Inbox({inbox, userData, chatOpened} : any) {
     const [searchVal, setSearchVal] = useState('')
     const [searchRes, setSearchRes] = useState([])
     const [selectedChat, setSelectedChat] = useState(-1)
@@ -14,7 +14,10 @@ export default function Inbox({inbox, userData} : any) {
     const [loadingMsg, setLoadingMsg] = useState(false)
     const [messageValue, setMessageValue] = useState('')
     const [chatSocket, setChatSocket] = useState(null)
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
 
+    const messagesContainerRef = useRef(null);
     useEffect(() => {
         if(searchVal.length > 0) {
             axios.get(`${process.env.SITE_URL}/searchfriends?q=${searchVal}`, {
@@ -32,9 +35,34 @@ export default function Inbox({inbox, userData} : any) {
     }
 
     useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1.0,
+          }
+          const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting) {
+              setPage((prevPage) => prevPage + 1);
+              console.log('kss')
+            }
+          }, options);
+      
+          if (messagesContainerRef.current) {
+            observer.observe(messagesContainerRef.current);
+          }
+      
+          return () => {
+            if (messagesContainerRef.current) {
+              observer.unobserve(messagesContainerRef.current);
+            }
+          };
+        }, [hasMore]);
+
+    useEffect(() => {
+        console.log('pagee', page)
         if(selectedChat >= 0) {
             setLoadingMsg(true)
-            axios.get(`${process.env.SITE_URL}/getchat?id=${selectedChat}`, {
+            axios.get(`${process.env.SITE_URL}/getchat?id=${selectedChat}&&page=${page}`, {
                 withCredentials:true
             }).then(res => {
                 setMessages(res.data)
@@ -52,7 +80,7 @@ export default function Inbox({inbox, userData} : any) {
                }
             }
         } 
-    }, [selectedChat])
+    }, [selectedChat, page])
 
   return (
     <>
@@ -79,11 +107,12 @@ export default function Inbox({inbox, userData} : any) {
                 {selectedChat < 0 && <span className={styles.empty}>Select a conversation</span>}
 
                 {selectedChat >= 0 &&  <div className={styles.chatUI}>
-                    <div className={styles.messagesContainer}>
+                    <div className={styles.messagesContainer} ref={messagesContainerRef}>
                         {loadingMsg ? <img className={styles.loadingMsg} src='/loading-melodyse.gif'/> : messages.length > 0 ? messages.map(msg => (
                             <Message message={msg} is_mine={msg.author_username == userData.username}/>
                             
                         )) : <span>Start a conversation</span>}
+                        {hasMore && <div className={styles.loadingMore}>Loading more messages...</div>}
                     </div>
                     <ChatInput value={messageValue} setValue={(val) => setMessageValue(val)} submit={sendMsg}/>
                     </div>}
